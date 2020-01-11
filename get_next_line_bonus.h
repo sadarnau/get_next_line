c82618da27c6cc1e0e76e44471bc0eb3634fe42e
Sami DARNAUD <sadarnau@e1r10p19.42.fr>
2020-01-11 18:15:19 +0100

    11/01/2020

---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadarnau <sadarnau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/13 20:06:04 by sadarnau          #+#    #+#             */
+/*   Updated: 2019/10/24 17:47:23 by sadarnau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2048
+# endif
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_last_line(char **ptr, char **line, int fd);
+int	get_next_line(int fd, char **line);
+
+#endif
